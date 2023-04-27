69febcd4a5c4cd33d02b17f59b89f0786e92cfd0
Rudruil <124600996+Rudruil@users.noreply.github.com>
2023-04-27 21:30:17 +0200

    Add files via upload

---
+++ Site_FlightRadar/script.js
@@ -0,0 +1,20 @@
+const createCursor =(x,y) =>{
+
+    const cursor = document.createElement('div');
+    cursor.ClassName = 'cursor';
+    cursor.style.left = `${x}px`;
+    cursor.style.top = `${y}px`;
+    return cursor;
+}
+
+const removeCursorFromDom = cursor =>{
+        const timeout = setTimeout(()=> {
+            cursor.remove();
+            clearTimeout(timeout);
+        }, 1000)
+}
+window.addEventListener('click',event =>{
+    const cursor = createCursor(event.pageX, event.pageY);
+    document.body.append(cursor)
+    removeCursorFromDom(cursor);
+})
